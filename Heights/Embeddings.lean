c257bd306901c448b5ef872aaa2ed0e2109e95d6
fbarroero <fbarroero@gmail.com>
2024-10-31 18:08:29 +0100

    Embeddings

--- Heights/Embeddings.lean
+++ Heights/Embeddings.lean
@@ -35,7 +35,31 @@ lemma one_lt_norm : 1 < (absNorm (v.asIdeal) : NNReal) := by
 
 lemma norm_ne_zero : (absNorm (v.asIdeal) : NNReal) ≠ 0 := ne_zero_of_lt (one_lt_norm v)
 
-noncomputable instance : Valuation.RankOne (IsDedekindDomain.HeightOneSpectrum.valuedAdicCompletion K v).v where
+noncomputable def vadic_abv : AbsoluteValue K ℝ where
+  toFun := fun x ↦ (toNNReal (norm_ne_zero v) (v.valuation x))
+  map_mul' := by
+    intro x y
+    simp_all only [_root_.map_mul, NNReal.coe_mul]
+  nonneg' := fun x ↦ NNReal.zero_le_coe
+  eq_zero' := by
+    intro x
+    simp_all only [NNReal.coe_eq_zero, map_eq_zero]
+  add_le' := by
+    intro x y
+    simp only
+    norm_cast
+    -- the triangular inequality is implied by the ultrametric one
+    apply le_trans _ <| max_le_add_of_nonneg (by simp only [zero_le]) (by simp only [zero_le])
+    have h_mono := StrictMono.monotone (WithZeroMulInt.toNNReal_strictMono (one_lt_norm v))
+    rw [← Monotone.map_max h_mono] --max goes inside withZeroMultIntToNNReal
+    exact h_mono (Valuation.map_add v.valuation x y)
+
+theorem vadic_abv_def : vadic_abv v x = (toNNReal (norm_ne_zero v) (v.valuation x)) := rfl
+
+
+
+
+noncomputable instance i1 : Valuation.RankOne (IsDedekindDomain.HeightOneSpectrum.valuedAdicCompletion K v).v where
   hom := {
     toFun := toNNReal (norm_ne_zero v)
     map_zero' := rfl
@@ -54,16 +78,10 @@ noncomputable instance : Valuation.RankOne (IsDedekindDomain.HeightOneSpectrum.v
       rw [IsDedekindDomain.HeightOneSpectrum.valuation_eq_intValuationDef, IsDedekindDomain.HeightOneSpectrum.intValuation_lt_one_iff_dvd]
       simp_all only [ne_eq, Ideal.dvd_span_singleton]
 
-noncomputable def vadic_abs_val : AbsoluteValue K ℝ where
-  toFun := fun x ↦ (toNNReal (norm_ne_zero v) (v.valuation x))
-  map_mul' := sorry
-  nonneg' := sorry
-  eq_zero' := sorry
-  add_le' := sorry
+noncomputable instance i2 : NormedField (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) :=
+    Valued.toNormedField (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) (WithZero (Multiplicative ℤ))
 
-noncomputable instance : NormedField (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) := Valued.toNormedField (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) (WithZero (Multiplicative ℤ))
-
-noncomputable instance : NormedDivisionRing (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) := NormedField.toNormedDivisionRing
+--noncomputable instance : NormedDivisionRing (IsDedekindDomain.HeightOneSpectrum.adicCompletion K v) := NormedField.toNormedDivisionRing
 
 /-- A finite place of a number field `K` is a place associated to an embedding into a completion with rescect to a maximal ideal. -/
 def NumberField.FinitePlace := { w : AbsoluteValue K ℝ //
@@ -72,7 +90,45 @@ def NumberField.FinitePlace := { w : AbsoluteValue K ℝ //
 
 /-- Return the finite place defined by a maximal ideal `v`. -/
 noncomputable def NumberField.FinitePlace.mk (v : IsDedekindDomain.HeightOneSpectrum (𝓞 K)) : @NumberField.FinitePlace K _ _ where
-  val := vadic_abs_val v
+  val := vadic_abv v
   property := by
     use v
+    let f : K →+* adicCompletion K v := {
+      toFun := fun x ↦ x
+      map_one' := rfl
+      map_mul' := by
+        simp only
+        intro x y
+        norm_cast
+      map_zero' := rfl
+      map_add' := by
+        simp only
+        intro x y
+        norm_cast
+    }
+    use f
+    have a := i1 v
+    have b := i2 v
+    ext x
+    simp only [place_apply, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk, f, norm,
+      NormedField.toNormedDivisionRing]
+    unfold norm
+    unfold NormedField.toNorm
+    simp [i2]
+
+
+
+    /-
+    simp only [place_apply, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk, vadic_abv_def, f]
+
+
+
+    --unfold NormedDivisionRing.toNorm
+    unfold norm
+    unfold NormedDivisionRing.toNorm
+    --simp_all only [RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk, f]
+    unfold NormedField.toNormedDivisionRing
+    unfold instNormedFieldAdicCompletionRingOfIntegers_heights
+     -/
+
     sorry
