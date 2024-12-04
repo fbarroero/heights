9a0135d52086371272491ef0e43e0898b5cdf741
Fabrizio Barroero <fbarroero@gmail.com>
2024-12-05 00:27:42 +0100

    finish bdd_coeff_of_bdd_roots_and_lead

--- Heights/MahlerMeasure.lean
+++ Heights/MahlerMeasure.lean
@@ -8,7 +8,6 @@ import Mathlib.FieldTheory.IsAlgClosed.Basic
 import Mathlib.RingTheory.Polynomial.Cyclotomic.Basic
 import Mathlib.RingTheory.Polynomial.Vieta
 import Mathlib.Tactic.ComputeDegree
-import Mathlib
 
 namespace Polynomial
 
@@ -74,6 +73,10 @@ theorem MM_mul (p q : Polynomial ℂ) : MahlerMeasure (p * q) = MahlerMeasure p 
       rw [roots_mul (mul_ne_zero hp hq)]
       simp only [Multiset.map_add, Multiset.prod_add]
 
+example {ι : Type u_1}  {M : Type u_4}  [OrderedCancelAddCommMonoid M]  {s : Finset ι}  {f g : ι → M} (h : ∀ i ∈ s, f i = g i) :
+∑ i ∈ s, f i = ∑ i ∈ s, g i := by
+  exact Finset.sum_congr rfl h
+
 theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B : NNReal}
     (h_bdd : (Multiset.map (fun (a : ℂ) ↦ ‖a‖₊) p.roots).sup ≤ B) :
     ∀ n, ‖p.coeff n‖₊ ≤ Nat.choose p.natDegree n * B ^ (p.natDegree - n) * ‖p.leadingCoeff‖₊ := by
@@ -91,7 +94,7 @@ theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B
   simp only [norm_mul, Complex.norm_natCast, Complex.norm_eq_abs, NNReal.val_eq_coe, NNReal.coe_mul,
     NNReal.coe_natCast, Finset.prod_multiset_count, Complex.abs_natCast, map_prod,
     AbsoluteValue.map_pow, NNReal.coe_pow]
-  have : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, ‖z‖₊ ≤ B := by
+  have h1 : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, ‖z‖₊ ≤ B := by
     intro x hx z hz
     apply h_bdd ‖z‖₊ z
     simp_all only [ne_eq, not_false_eq_true, true_and, and_imp, forall_apply_eq_imp_iff₂,
@@ -100,6 +103,13 @@ theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B
     have : z ∈ p.roots := Multiset.mem_of_le left hz
     simp only [mem_roots', ne_eq, IsRoot.def] at this
     exact this.2
+  have h2 : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, Multiset.count z x ≤ p.natDegree - n := by
+    intro x hx z hz
+    simp_all only [ne_eq, not_false_eq_true, true_and, and_imp, forall_apply_eq_imp_iff₂, leadingCoeff_eq_zero,
+      Multiset.mem_toFinset, Multiset.mem_powersetCard]
+    obtain ⟨left, right⟩ := hx
+    rw [← right]
+    exact Multiset.count_le_card z x
   calc
   ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
     ↑(Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) *
@@ -107,11 +117,53 @@ theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B
   ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
     ↑(Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) *
       ∏ x_1 ∈ x.toFinset, (B : ℝ) ^ Multiset.count x_1 x := by
-      gcongr
-     -- exact this i✝ a✝ i✝¹ a✝¹
-      sorry
-  _ ≤ ↑(p.natDegree.choose n) * ↑B ^ (p.natDegree - n) := by sorry
+      gcongr with x hx z hz
+      exact h1 x hx z hz
+  _ = ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
+    ↑(Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) *
+    (B : ℝ) ^ (p.natDegree - n) := by
+      apply Finset.sum_congr rfl
+      intro x hx
+      simp_all only [ne_eq, not_false_eq_true, true_and, and_imp, forall_apply_eq_imp_iff₂, leadingCoeff_eq_zero,
+        Multiset.mem_toFinset, Multiset.mem_powersetCard, mul_eq_mul_left_iff, Nat.cast_eq_zero,
+        Multiset.count_eq_zero, and_self, not_true_eq_false, or_false]
+      obtain ⟨left, right⟩ := hx
+      rw [Finset.prod_pow_eq_pow_sum]
+      congr
+      simp_all only [Multiset.mem_toFinset, implies_true, Multiset.sum_count_eq_card]
+  _ ≤ ↑(p.natDegree.choose n) * ↑B ^ (p.natDegree - n) := by
+    by_cases hB : B = 0
+    by_cases hd : p.natDegree - n = 0
+    · simp only [hd, Multiset.powersetCard_zero_left, Multiset.toFinset_singleton,
+      Multiset.nodup_singleton, hB, NNReal.coe_zero, pow_zero, mul_one, Finset.sum_singleton,
+      Multiset.mem_singleton, Multiset.count_eq_one_of_mem, Nat.cast_one, Nat.one_le_cast]
+      rw [Nat.le_antisymm h <| Nat.le_of_sub_eq_zero hd, Nat.choose_self]
+    · simp only [hB, NNReal.coe_zero, ne_eq, hd, not_false_eq_true, zero_pow, mul_zero,
+      Finset.sum_const_zero, le_refl]
+    rw [← Finset.sum_mul, mul_le_mul_right (mod_cast pow_pos (pos_iff_ne_zero.mpr hB) _)]
+    norm_cast
+    simp only [Multiset.mem_powersetCard, Multiset.mem_toFinset, imp_self, implies_true,
+      Multiset.sum_count_eq_card, Multiset.card_powersetCard]
+    let i : ℂ →+* ℂ := {
+      toFun := id
+      map_one' := rfl
+      map_mul' _ _ := rfl
+      map_zero' := rfl
+      map_add' _ _:= rfl
+    }
+    have h3: Multiset.card p.roots = p.natDegree := by
+      rw [natDegree_eq_card_roots (i:=i) (IsAlgClosed.splits_domain p)]
+      simp only [i]
+      congr
+      ext n
+      simp only [coeff_map, RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk, id_eq]
+    rw [h3]
+    apply le_of_eq
+    simp only [h, Nat.choose_symm]
 
+
+
+/-
 
 --change to complex poly
 theorem bdd_coeff_of_bdd_roots_and_lead' {p : Polynomial ℤ} (h₀ : p ≠ 0) {B : NNReal}
@@ -159,19 +211,30 @@ theorem bdd_coeff_of_bdd_roots_and_lead' {p : Polynomial ℤ} (h₀ : p ≠ 0) {
     sorry
     intro y hy
     sorry
-  sorry
+  sorry -/
 
 
 theorem Kronecker {p : Polynomial ℤ} (h_monic : Monic p) (h_irr : Irreducible p)
     (h_MM : MahlerMeasure (map coe p) = 1) : p = X ∨ ∃ n : ℕ, p = cyclotomic n ℤ := by
-  by_cases h : p = X; left; exact h; right
+  rw [or_iff_not_imp_left]
+  intro hp_ne_X
+  by_cases h_deg_p : p.natDegree = 0; use 0; simp_all only [Monic.natDegree_eq_zero_iff_eq_one, cyclotomic_zero]
   use natDegree p
   convert_to map coe p = map coe (cyclotomic p.natDegree ℤ)
   refine ⟨fun a ↦ congrArg (map coe) a, fun a ↦ map_injective coe (RingHom.injective_int coe) a⟩
-
   simp only [map_cyclotomic]
+  have : IsPrimitiveRoot (Complex.exp (2 * Real.pi * Complex.I * (1 / p.natDegree))) p.natDegree := by
+    rw [Complex.isPrimitiveRoot_iff _ _ h_deg_p]
+
+    sorry
+
+
+
+
   rw [cyclotomic_eq_prod_X_sub_primitiveRoots]
+
   sorry
+
   sorry
   sorry
 -- https://mathoverflow.net/questions/10911/english-reference-for-a-result-of-kronecker
