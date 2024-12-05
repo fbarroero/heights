89e8db0639262cb27a1bde0ec572a8c7b54453ae
fbarroero <fbarroero@gmail.com>
2024-12-05 19:45:16 +0100

    generalization bdd_coeff_of_bdd_roots_and_lead almost done

--- Heights/MahlerMeasure.lean
+++ Heights/MahlerMeasure.lean
@@ -73,9 +73,68 @@ theorem MM_mul (p q : Polynomial ℂ) : MahlerMeasure (p * q) = MahlerMeasure p 
       rw [roots_mul (mul_ne_zero hp hq)]
       simp only [Multiset.map_add, Multiset.prod_add]
 
-example {ι : Type u_1}  {M : Type u_4}  [OrderedCancelAddCommMonoid M]  {s : Finset ι}  {f g : ι → M} (h : ∀ i ∈ s, f i = g i) :
-∑ i ∈ s, f i = ∑ i ∈ s, g i := by
-  exact Finset.sum_congr rfl h
+open Classical
+
+theorem bdd_coeff_of_bdd_roots_and_lead' {K : Type*} [NormedField K]
+    [IsAlgClosed K] [CharZero K] {p : Polynomial K} (h₀ : p ≠ 0) {B : NNReal}
+    (h_bdd : (Multiset.map (fun (a : K) ↦ ‖a‖₊) p.roots).sup ≤ B) :
+    ∀ n, ‖p.coeff n‖₊ ≤ Nat.choose p.natDegree n * B ^ (p.natDegree - n) * ‖p.leadingCoeff‖₊ := by
+  intro n
+  have h_lcoeff : p.leadingCoeff ≠ 0  := leadingCoeff_ne_zero.mpr h₀
+  by_cases h : p.natDegree < n; simp only [coeff_eq_zero_of_natDegree_lt h, nnnorm_zero, zero_le]
+  rw [not_lt] at h
+  simp only [coeff_eq_esymm_roots_of_card (splits_iff_card_roots.mp (IsAlgClosed.splits_codomain p))
+    h, Multiset.esymm, Finset.sum_multiset_map_count, nnnorm_mul, nnnorm_pow, nnnorm_neg,
+    nnnorm_one, one_pow, mul_one, mul_comm _ ‖p.leadingCoeff‖₊, mul_le_mul_left
+    (nnnorm_pos.mpr h_lcoeff), nsmul_eq_mul]
+  apply le_trans (nnnorm_sum_le _ _)
+  simp_rw [Finset.prod_multiset_count, nnnorm_mul, nnnorm_prod, nnnorm_pow]
+  simp only [Multiset.sup_le, Multiset.mem_map, mem_roots', IsRoot.def, forall_exists_index,
+    and_imp] at h_bdd
+  have h1 : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, ‖z‖₊ ≤ B := by
+    intro x hx z hz
+    apply h_bdd ‖z‖₊ z h₀ _ rfl
+    simp only [Multiset.mem_toFinset, Multiset.mem_powersetCard] at hx
+    obtain ⟨h_root, _⟩ := hx
+    have : z ∈ p.roots := Multiset.mem_of_le h_root (Multiset.mem_dedup.mp hz)
+    rw [mem_roots', IsRoot.def] at this
+    exact this.2
+  calc
+      ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
+        ‖((Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) : K)‖₊ *
+        ∏ x_1 ∈ x.toFinset, ‖x_1‖₊ ^ Multiset.count x_1 x
+    ≤ ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
+        ‖((Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) : K)‖₊ *
+        ∏ x_1 ∈ x.toFinset, B ^ Multiset.count x_1 x := by
+          gcongr with x hx z hz
+          exact h1 x hx z hz
+  _ = ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
+        ‖((Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) : K)‖₊ *
+        B ^ (p.natDegree - n) := by
+          apply Finset.sum_congr rfl
+          intro x hx
+          simp only [mul_eq_mul_left_iff, nnnorm_eq_zero, Nat.cast_eq_zero,
+            Multiset.count_eq_zero, Multiset.mem_powersetCard, not_and, Finset.prod_pow_eq_pow_sum]
+          left
+          congr
+          simp_all only [Multiset.mem_toFinset, Multiset.mem_powersetCard, implies_true,
+              Multiset.sum_count_eq_card]
+  _ = ↑(p.natDegree.choose n) * B ^ (p.natDegree - n) := by
+          by_cases hB : B = 0
+          by_cases hd : p.natDegree - n = 0
+          · simp only [hd, Multiset.powersetCard_zero_left, Multiset.toFinset_singleton,
+              Multiset.nodup_singleton, hB, pow_zero, mul_one, Finset.sum_singleton,
+              Multiset.mem_singleton, Multiset.count_eq_one_of_mem, Nat.cast_one, nnnorm_one]
+            rw [Nat.le_antisymm h <| Nat.le_of_sub_eq_zero hd, Nat.choose_self, Nat.cast_one]
+          · simp only [hB, NNReal.coe_zero, ne_eq, hd, not_false_eq_true, zero_pow, mul_zero,
+              Finset.sum_const_zero]
+          ·
+            sorry
+
+
+
+
+
 
 theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B : NNReal}
     (h_bdd : (Multiset.map (fun (a : ℂ) ↦ ‖a‖₊) p.roots).sup ≤ B) :
@@ -103,13 +162,13 @@ theorem bdd_coeff_of_bdd_roots_and_lead {p : Polynomial ℂ} (h₀ : p ≠ 0) {B
     have : z ∈ p.roots := Multiset.mem_of_le left hz
     simp only [mem_roots', ne_eq, IsRoot.def] at this
     exact this.2
-  have h2 : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, Multiset.count z x ≤ p.natDegree - n := by
+/-   have h2 : ∀ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset, ∀ z ∈ x.toFinset, Multiset.count z x ≤ p.natDegree - n := by
     intro x hx z hz
     simp_all only [ne_eq, not_false_eq_true, true_and, and_imp, forall_apply_eq_imp_iff₂, leadingCoeff_eq_zero,
       Multiset.mem_toFinset, Multiset.mem_powersetCard]
     obtain ⟨left, right⟩ := hx
     rw [← right]
-    exact Multiset.count_le_card z x
+    exact Multiset.count_le_card z x -/
   calc
   ∑ x ∈ (Multiset.powersetCard (p.natDegree - n) p.roots).toFinset,
     ↑(Multiset.count x (Multiset.powersetCard (p.natDegree - n) p.roots)) *
