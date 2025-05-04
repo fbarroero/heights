c49d62c5e06606981f14771bb55a1d91fca3c9cd
fbarroero <fbarroero@gmail.com>
2025-05-04 17:26:48 +0200

    GaussNormSeries

--- Heights/GaussNormSeries.lean
+++ Heights/GaussNormSeries.lean
@@ -1,28 +1,34 @@
 import Mathlib
 
+variable {R F : Type*} [Semiring R] [FunLike F R ℝ] (v : F) (c : ℝ)
 namespace PowerSeries
-
-variable {R : Type*} [Semiring R] --[Semiring α] [LinearOrder α]
- (v : AbsoluteValue R ℝ) (hna : IsNonarchimedean v) (c : ℝ)
 
 noncomputable def gaussNormC (f : PowerSeries R) : ℝ := sSup {v (f.coeff R i) * c ^ i | (i : ℕ)}
 
+--#find_home! gaussNormC
+--gives [Mathlib.NumberTheory.ZetaValues, Mathlib.NumberTheory.ModularForms.QExpansion]
 @[simp]
-theorem gaussNormC_zero : gaussNormC v c 0 = 0 := by simp [gaussNormC]
+theorem gaussNormC_zero [ZeroHomClass F R ℝ] : gaussNormC v c 0 = 0 := by simp [gaussNormC]
 
 end PowerSeries
 
 namespace Polynomial
 
-variable {R : Type*} [Semiring R] (v : AbsoluteValue R ℝ) (hna : IsNonarchimedean v) (c : ℝ)
-
 def gaussNormC (p : R[X]) : ℝ := if h : p.support.Nonempty then p.support.sup' h fun i ↦
     (v (p.coeff i) * c ^ i) else 0
 
 @[simp]
-theorem gaussNorm_zero : gaussNormC v c 0 = 0 := by simp [gaussNormC]
+theorem gaussNormC_zero : gaussNormC v c 0 = 0 := by simp [gaussNormC]
 
-theorem eq (p : R[X]) : p.gaussNormC v c = (p.toPowerSeries).gaussNormC v c  := by
+@[simp]
+lemma gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : gaussNormC v c (C r) = v r := by
+  by_cases hr : r = (0 : R); simp [gaussNormC, hr]
+  simp [gaussNormC, support_C, hr]
+
+  --simp [gaussNormC, support_C]
+
+@[simp]
+theorem eq [ZeroHomClass F R ℝ] (p : R[X]) : (p.toPowerSeries).gaussNormC v c = p.gaussNormC v c:= by
   by_cases hp : p = 0; simp [hp]
   have h_supp : p.support.Nonempty := support_nonempty.mpr hp
   have h : (p.toPowerSeries).gaussNormC v c ≤ p.gaussNormC v c := by
@@ -43,6 +49,7 @@ theorem eq (p : R[X]) : p.gaussNormC v c = (p.toPowerSeries).gaussNormC v c  := 
   simp only [gaussNormC, support_nonempty, ne_eq, hp, not_false_eq_true, ↓reduceDIte,
     AbsoluteValue.map_mul, PowerSeries.gaussNormC, coeff_coe]
   apply le_antisymm
+  · simp_all [PowerSeries.gaussNormC, gaussNormC, h_supp, -Finset.le_sup'_iff]
   · rw [Real.le_sSup_iff]
     · sorry
     · use p.gaussNormC v c
@@ -53,10 +60,6 @@ theorem eq (p : R[X]) : p.gaussNormC v c = (p.toPowerSeries).gaussNormC v c  := 
       obtain ⟨i, hi⟩ := hx
       rw [← hi]
       --should be able to finish
-
-
-
-
 /-
       simp [GaussNormC, h_supp]
       rw [mem_upperBounds]
@@ -65,21 +68,26 @@ theorem eq (p : R[X]) : p.gaussNormC v c = (p.toPowerSeries).gaussNormC v c  := 
       rw [← hi] -/
       sorry
     · use v (p.coeff 0) * c ^ 0, 0
-  · simp_all [PowerSeries.gaussNormC, gaussNormC, h_supp, -Finset.le_sup'_iff]
+
 
 end Polynomial
 
+@[simp]
+theorem PowerSeries.gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : gaussNormC v c (C R r) = v r := by
+  simp [← @Polynomial.coe_C]
+
+
 section Coram
 
-variable (c : ℝ) (R : Type*) [NormedRing R] [IsUltrametricDist R]
+variable (c : ℝ) (R F : Type*) [NormedRing R] --[IsUltrametricDist R]
 
 noncomputable def gaussNormC' : PowerSeries R → ℝ :=
   fun f => sSup {‖f.coeff R i‖ * c ^ i | (i : ℕ)}
 
-theorem gaussNormC'_zero : gaussNormC' c R 0 = 0 := by simp [gaussNormC']
+--theorem gaussNormC'_zero : gaussNormC' c R 0 = 0 := by simp [gaussNormC']
 
-theorem gaussNormC'_eq (f : PowerSeries R) : gaussNormC' c R f = f.gaussNormC v c := by
+theorem gaussNormC'_eq (f : PowerSeries R) : gaussNormC' c R f = f.gaussNormC (NormedRing.toRingNorm R) c := by
+  simp [gaussNormC', PowerSeries.gaussNormC]
 
-  sorry
 
 end Coram
--- Heights/Mwe.lean
+++ Heights/Mwe.lean
@@ -1,14 +1,46 @@
 import Mathlib
 
+variable {R F : Type*} [Semiring R] [FunLike F R ℝ] (v : F) (c : ℝ)
+namespace PowerSeries
+
+noncomputable def gaussNormC (f : PowerSeries R) : ℝ := sSup {v (f.coeff R i) * c ^ i | (i : ℕ)}
+
+@[simp]
+lemma gaussNormC_zero [ZeroHomClass F R ℝ] : gaussNormC v c 0 = 0 := by simp [gaussNormC]
+
+end PowerSeries
+
 namespace Polynomial
 
-variable {R α : Type*} [Semiring R] [Semiring α] [LinearOrder α]
- (f : AbsoluteValue R α) (hna : IsNonarchimedean f)
+def gaussNormC (p : R[X]) : ℝ := if h : p.support.Nonempty then p.support.sup' h fun i ↦
+    (v (p.coeff i) * c ^ i) else 0
 
-def GaussNorm : R[X] → α := fun p ↦ if h : p.support.Nonempty then p.support.sup' h fun i ↦
-    (f (p.coeff i)) else 0
+@[simp]
+lemma gaussNormC_zero : gaussNormC v c 0 = 0 := by simp [gaussNormC]
 
-include hna in
-theorem isNonarchimedean_GaussNorm [AddLeftMono α] : IsNonarchimedean (GaussNorm f) := sorry
+@[simp]
+lemma gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : (C r).gaussNormC v c = v r := by
+  by_cases hr : r = (0 : R); simp [gaussNormC, hr]
+  simp [gaussNormC, support_C, hr]
+
+@[simp]
+theorem gaussNormC_toPowerSeries [ZeroHomClass F R ℝ] (p : R[X]) : (p.toPowerSeries).gaussNormC v c = p.gaussNormC v c := by
+  sorry
 
 end Polynomial
+
+@[simp]
+theorem PowerSeries.gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : (C R r).gaussNormC v c = v r := by
+  simp [← @Polynomial.coe_C]
+
+section NormedRing
+
+variable (c : ℝ) (R F : Type*) [NormedRing R]
+
+noncomputable def gaussNormC' : PowerSeries R → ℝ :=
+  fun f => sSup {‖f.coeff R i‖ * c ^ i | (i : ℕ)}
+
+theorem gaussNormC'_eq (f : PowerSeries R) : gaussNormC' c R f = f.gaussNormC (NormedRing.toRingNorm R) c := by
+  simp [gaussNormC', PowerSeries.gaussNormC]
+
+end NormedRing
