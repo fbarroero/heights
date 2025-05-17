ba5ac34a4808db8f4221bbf829c1f4f958289542
fbarroero <fbarroero@gmail.com>
2025-05-17 11:34:17 +0200

    PRd

--- Heights/GaussNormC/Polynomial/GaussNormC.lean
+++ Heights/GaussNormC/Polynomial/GaussNormC.lean
@@ -3,59 +3,62 @@ Copyright (c) 2025 Fabrizio Barroero. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fabrizio Barroero
 -/
+--import Mathlib
 import Heights.GaussNormC.PowerSeries.GaussNormC
+/-!
+# Gauss norm for polynomials
+This file defines the Gauss norm for polynomials. Given a polynomial `p` in `R[X]`, a function
+`v : R → ℝ` and a real number `c`, the Gauss norm is defined as the supremum of the set of all
+values of `v (p.coeff i) * c ^ i` for all `i` in the support of `p`.
 
+
+## Main Definitions and Results
+* `Polynomial.gaussNormC` is the supremum of the set of all values of `v (p.coeff i) * c ^ i`
+  for all `i` in the support of `p`, where `p` is a polynomial in `R[X]`, `v : R → ℝ` is a function
+  and `c` is a  real number.
+* `Polynomial.gaussNormC_coe_powerSeries`: the Gauss norm of a polynomial is equal to its
+  Gauss norm as a power series.
+* `Polynomial.gaussNormC_nonneg`: if `v` is a non-negative function, then the Gauss norm is
+  non-negative.
+* `Polynomial.gaussNormC_eq_zero_iff`: if `v x = 0 ↔ x = 0` for all `x : R`, then the Gauss
+  norm is zero if and only if the polynomial is zero.
+-/
 variable {R F : Type*} [Semiring R] [FunLike F R ℝ] (v : F) (c : ℝ)
 
 namespace Polynomial
 
+variable (p : R[X])
+
 /-- -/
-def gaussNormC (p : R[X]) : ℝ := if h : p.support.Nonempty then p.support.sup' h fun i ↦
+def gaussNormC : ℝ := if h : p.support.Nonempty then p.support.sup' h fun i ↦
     (v (p.coeff i) * c ^ i) else 0
 
 @[simp]
 lemma gaussNormC_zero : gaussNormC v c 0 = 0 := by simp [gaussNormC]
 
-private lemma sup'_nonneg_of_ne_zero {p : R[X]} (h : p.support.Nonempty) (hc : 0 ≤ c)
-    [NonnegHomClass F R ℝ]: 0 ≤ p.support.sup' h fun i ↦ (v (p.coeff i) * c ^ i) := by
+theorem exists_eq_gaussNormC [ZeroHomClass F R ℝ] : ∃ i, p.gaussNormC v c = v (p.coeff i) * c ^ i := by
+  by_cases h_supp : p.support.Nonempty
+  · simp only [gaussNormC, h_supp]
+    obtain ⟨i, hi1, hi2⟩ := Finset.exists_mem_eq_sup' h_supp fun i ↦ (v (p.coeff i) * c ^ i)
+    exact ⟨i, hi2⟩
+  · simp_all
+
+private lemma sup'_nonneg_of_ne_zero [NonnegHomClass F R ℝ] {p : R[X]} (h : p.support.Nonempty)
+    {c : ℝ} (hc : 0 ≤ c) : 0 ≤ p.support.sup' h fun i ↦ (v (p.coeff i) * c ^ i) := by
   simp only [Finset.le_sup'_iff, mem_support_iff]
   use p.natDegree
   simp_all only [support_nonempty, ne_eq, coeff_natDegree, leadingCoeff_eq_zero, not_false_eq_true,
     true_and]
   positivity
 
-@[simp]
-theorem gaussNormC_nonneg (p : R[X]) {c : ℝ} (hc : 0 ≤ c) [NonnegHomClass F R ℝ] :
-    0 ≤ p.gaussNormC v c := by
-  by_cases hp : p.support.Nonempty
-  · simp_all [gaussNormC, sup'_nonneg_of_ne_zero, -Finset.le_sup'_iff]
-  · simp [gaussNormC, hp]
-
-@[simp]
-lemma gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : (C r).gaussNormC v c = v r := by
-  by_cases hr : r = (0 : R); simp [gaussNormC, hr]
-  simp [gaussNormC, support_C, hr]
-
-lemma le_gaussNormC [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] (p : R[X]) (i : ℕ) {c : ℝ}
-    (hc : 0 ≤ c) : v (p.coeff i) * c ^ i ≤ p.gaussNormC v c := by
-  by_cases hp : p = 0; simp [hp]
-  by_cases hi : ¬ i ∈ p.support; simp_all
-  simp only [mem_support_iff, not_not] at hi
-  simp only [gaussNormC, support_nonempty, ne_eq, hp, not_false_eq_true, ↓reduceDIte,
-    Finset.le_sup'_iff, mem_support_iff]
-  use i
-
-private lemma aux_bdd [ZeroHomClass F R ℝ] (p : R[X]) :
-    BddAbove {x | ∃ i, v (p.coeff i) * c ^ i = x} := by
-  apply Set.Finite.bddAbove
+private lemma aux_bdd [ZeroHomClass F R ℝ] : BddAbove {x | ∃ i, v (p.coeff i) * c ^ i = x} := by
   let f : p.support → ℝ := fun i ↦ v (p.coeff i) * c ^ i.1
-  let t := f '' ⊤ ∪ {0}
-  have h_fin : t.Finite := by
+  have h_fin : (f '' ⊤ ∪ {0}).Finite := by
     apply Set.Finite.union _ <| Set.finite_singleton 0
     apply Set.Finite.image f
     rw [Set.top_eq_univ, Set.finite_univ_iff, ← @Finset.coe_sort_coe]
     exact Finite.of_fintype p.support
-  apply Set.Finite.subset h_fin
+  apply Set.Finite.bddAbove <| Set.Finite.subset h_fin _
   intro x hx
   obtain ⟨i, hi⟩ := hx
   rw [← hi]
@@ -66,34 +69,45 @@ private lemma aux_bdd [ZeroHomClass F R ℝ] (p : R[X]) :
   · right
     simp [not_mem_support_iff.mp hi]
 
-lemma gaussNormC_nonempty (p : R[X]) : {x | ∃ i, v (p.coeff i) * c ^ i = x}.Nonempty := by
+private lemma gaussNormC_nonempty : {x | ∃ i, v (p.coeff i) * c ^ i = x}.Nonempty := by
   use v (p.coeff 0) * c ^ 0, 0
 
-theorem gaussNormC_coe_powerSeries [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ ] (p : R[X])
-    {c : ℝ} (hc : 0 ≤ c) :
-    p.gaussNormC v c = (p.toPowerSeries).gaussNormC v c := by
+@[simp]
+theorem gaussNormC_coe_powerSeries [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]
+    {c : ℝ} (hc : 0 ≤ c) : (p.toPowerSeries).gaussNormC v c = p.gaussNormC v c:= by
   by_cases hp : p = 0; simp [hp]
   have h_supp : p.support.Nonempty := support_nonempty.mpr hp
   simp only [gaussNormC, support_nonempty, ne_eq, hp, not_false_eq_true, ↓reduceDIte,
     PowerSeries.gaussNormC, coeff_coe]
   apply le_antisymm
-  · rw [Real.le_sSup_iff (aux_bdd v c p) (gaussNormC_nonempty v c p)]
-    sorry
-  · apply Real.sSup_le
+  · apply Real.sSup_le  _ <| sup'_nonneg_of_ne_zero v h_supp hc
     intro x hx
     obtain ⟨i, hi⟩ := hx
     rw [← hi]
+    by_cases h : i ∈ p.support
+    · exact Finset.le_sup' (fun j ↦ v (p.coeff j) * c ^ j) h
+    · simp_all [sup'_nonneg_of_ne_zero v h_supp hc]
+  · obtain ⟨i, hi⟩ := exists_eq_gaussNormC v c p
+    simp only [gaussNormC, h_supp, ↓reduceDIte] at hi
+    rw [hi]
+    apply le_csSup (aux_bdd v c p)
+    simp
 
-    --simp [sSup, -Finset.le_sup'_iff, mem_support_iff, ne_eq, aux_bdd v c p]
-    /-
+@[simp]
+theorem gaussNormC_eq_zero_iff [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]
+    (h_eq_zero : ∀ x : R, v x = 0 → x = 0) {c : ℝ} (hc : 0 < c) : p.gaussNormC v c = 0 ↔ p = 0 := by
+  rw [← gaussNormC_coe_powerSeries _ _ (le_of_lt hc), PowerSeries.gaussNormC_eq_zero_iff h_eq_zero hc
+    (by simpa only [coeff_coe] using aux_bdd v c p), coe_eq_zero_iff]
 
+theorem gaussNormC_nonneg {c : ℝ} (hc : 0 ≤ c) [NonnegHomClass F R ℝ] : 0 ≤ p.gaussNormC v c := by
+  by_cases hp : p.support.Nonempty
+  · simp_all [gaussNormC, sup'_nonneg_of_ne_zero, -Finset.le_sup'_iff]
+  · simp [gaussNormC, hp]
 
-    apply Real.sSup_le
-    intro x hx
-    obtain ⟨i, hi⟩ := hx -/
-    sorry
-    sorry
-
+@[simp]
+lemma gaussNormC_C [ZeroHomClass F R ℝ] (r : R) : (C r).gaussNormC v c = v r := by
+  by_cases hr : r = (0 : R); simp [gaussNormC, hr]
+  simp [gaussNormC, support_C, hr]
 
 @[simp]
 theorem gaussNormC_monomial [ZeroHomClass F R ℝ] (n : ℕ) (r : R) :
@@ -101,37 +115,20 @@ theorem gaussNormC_monomial [ZeroHomClass F R ℝ] (n : ℕ) (r : R) :
   by_cases hr : r = 0; simp [gaussNormC, hr]
   simp [gaussNormC, support_monomial, hr]
 
-@[simp]
-theorem gaussNormC_eq_zero_iff [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]  (h_eq_zero : ∀ x : R, v x = 0 ↔ x = 0)
-    (p : R[X]) {c : ℝ} (hc : 0 < c) : p.gaussNormC v c = 0 ↔ p = 0 := by
-  rw [gaussNormC_coe_powerSeries _ _ (le_of_lt hc), PowerSeries.gaussNormC_eq_zero_iff h_eq_zero hc, coe_eq_zero_iff]
-  simp only [coeff_coe]
-  exact aux_bdd v c p
-/-
-  simp only [gaussNormC, support_nonempty, dite_eq_right_iff]
-  refine ⟨?_, fun _ _ ↦ by simp_all⟩
-  contrapose!
-  intro hp
-  simp_all only [ne_eq, not_false_eq_true, exists_true_left]
-  apply ne_of_gt
-  calc
-  0 < v (p.coeff p.natDegree) * c ^ p.natDegree := by
-    apply mul_pos _ (pow_pos hc _)
-    specialize h_eq_zero (p.coeff p.natDegree)
-    simp_all only [coeff_natDegree, leadingCoeff_eq_zero, iff_false]
-    positivity
-  _ ≤ p.support.sup' (support_nonempty.mpr hp) fun i ↦ v (p.coeff i) * c ^ i :=
-    Finset.le_sup' (fun i ↦ v (p.coeff i) * c ^ i) (natDegree_mem_support_of_nonzero hp) -/
+lemma le_gaussNormC [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] (p : R[X]) {c : ℝ}
+    (hc : 0 ≤ c) (i : ℕ) : v (p.coeff i) * c ^ i ≤ p.gaussNormC v c := by
+  rw [← gaussNormC_coe_powerSeries _ _ hc, ← coeff_coe]
+  apply PowerSeries.le_gaussNormC
+  simpa using aux_bdd v c p
 
-theorem isNonarchimedean_gaussNormC [NonnegHomClass F R ℝ]
-    (hna : IsNonarchimedean v)
-    (p : R[X]) {c : ℝ} (hc : 0 ≤ c) :
-    IsNonarchimedean (gaussNormC v c) := by
+/- theorem isNonarchimedean_gaussNormC [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]
+    (hna : IsNonarchimedean v) {c : ℝ} (hc : 0 ≤ c) : IsNonarchimedean (gaussNormC v c) := by
   intro p q
   by_cases hp : p = 0; simp [hp]
   by_cases hq : q = 0; simp [hq]
-  by_cases hpq : p + q = 0; simp [hpq, hc]
-  simp only [gaussNormC, support_nonempty, ne_eq, hpq, not_false_eq_true, ↓reduceDIte, coeff_add,
+  by_cases hpq : p + q = 0; simp [hpq, hc, gaussNormC_nonneg]
+  rw [gaussNormC]
+  simp only [support_nonempty, ne_eq, hpq, not_false_eq_true, ↓reduceDIte, coeff_add,
     hp, hq, Finset.sup'_le_iff, mem_support_iff]
   intro i hi
   calc
@@ -140,17 +137,34 @@ theorem isNonarchimedean_gaussNormC [NonnegHomClass F R ℝ]
     exact hna (p.coeff i) (q.coeff i)
   _ = max (v (p.coeff i) * c ^ i) (v (q.coeff i) * c ^ i) := by
     rw [max_mul_of_nonneg _ _ (pow_nonneg hc _)]
-  _ ≤ max (p.support.sup' (support_nonempty.mpr hp) fun i ↦ v (p.coeff i) * c ^ i)
-      (q.support.sup' (support_nonempty.mpr hq) fun i ↦ v (q.coeff i) * c ^ i) := by
+  _ ≤ max (gaussNormC v c p) (gaussNormC v c q) := by
     apply max_le_max <;>
-    sorry
+    exact le_gaussNormC v _ hc i -/
 
+end Polynomial
 
+namespace PowerSeries
+
+@[simp]
+theorem gaussNormC_C [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] {c : ℝ} (hc : 0 ≤ c) (r : R) :
+    (C R r).gaussNormC v c = v r := by
+  simp [← @Polynomial.coe_C, hc]
+
+@[simp]
+theorem gaussNormC_monomial [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] {c : ℝ} (hc : 0 ≤ c)
+    (n : ℕ) (r : R) : (monomial R n r).gaussNormC v c = v r * c ^ n := by
+  simp [← @Polynomial.coe_monomial, hc]
+
+end PowerSeries
+
+--#min_imports
+
+namespace Polynomial
 section AbsoluteValue
 
 variable (v : AbsoluteValue R ℝ)
 
-
+--TODO
 
 end AbsoluteValue
 
--- Heights/GaussNormC/PowerSeries/GaussNormC.lean
+++ Heights/GaussNormC/PowerSeries/GaussNormC.lean
@@ -3,40 +3,61 @@ Copyright (c) 2025 Fabrizio Barroero. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fabrizio Barroero
 -/
-import Mathlib
+import Mathlib.Analysis.Normed.Ring.Basic
+import Mathlib.RingTheory.PowerSeries.Order
+/-!
+# Gauss norm for power series
+This file defines the Gauss norm for power series. Given a power series `f` in `R⟦X⟧`, a function
+`v : R → ℝ` and a real number `c`, the Gauss norm is defined as the supremum of the set of all
+values of `v (f.coeff R i) * c ^ i` for all `i : ℕ`.
 
-variable {R F : Type*} [Semiring R] [FunLike F R ℝ] (v : F) (c : ℝ)
+
+## Main Definitions and Results
+* `PowerSeries.gaussNormC` is the supremum of the set of all values of `v (f.coeff R i) * c ^ i`
+  for all `i : ℕ`, where `f` is a power series in `R⟦X⟧`, `v : R → ℝ` is a function and `c` is a
+  real number.
+* `PowerSeries.gaussNormC_nonneg`: if `v` is a non-negative function, then the Gauss norm is
+  non-negative.
+* `PowerSeries.gaussNormC_eq_zero_iff`: if `v x = 0 ↔ x = 0` for all `x : R`, then the Gauss
+  norm is zero if and only if the power series is zero.
+-/
 
 namespace PowerSeries
 
-noncomputable def gaussNormC (f : R⟦X⟧) : ℝ := sSup {v (f.coeff R i) * c ^ i | (i : ℕ)}
+variable {R F : Type*} [Semiring R] [FunLike F R ℝ] (v : F) (c : ℝ) (f : R⟦X⟧)
+
+noncomputable def gaussNormC : ℝ := sSup {v (f.coeff R i) * c ^ i | (i : ℕ)}
 
 @[simp]
 theorem gaussNormC_zero [ZeroHomClass F R ℝ] : gaussNormC v c 0 = 0 := by simp [gaussNormC]
 
-lemma gaussNormC_nonempty (f : R⟦X⟧) : {x | ∃ i, v ((coeff R i) f) * c ^ i = x}.Nonempty := by
+private lemma gaussNormC_nonempty : {x | ∃ i, v (f.coeff R i) * c ^ i = x}.Nonempty := by
   use v (f.coeff R 0) * c ^ 0, 0
 
-@[simp]
-theorem gaussNormC_nonneg (f : R⟦X⟧) [NonnegHomClass F R ℝ] : 0 ≤ f.gaussNormC v c := by
-  simp [gaussNormC]
+lemma le_gaussNormC [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]
+    (hbd : BddAbove {x | ∃ i, v (f.coeff R i) * c ^ i = x}) (i : ℕ) :
+  v (f.coeff R i) * c ^ i ≤ f.gaussNormC v c := by
+  apply le_csSup hbd
+  simp
+
+theorem gaussNormC_nonneg [NonnegHomClass F R ℝ] : 0 ≤ f.gaussNormC v c := by
+  rw [gaussNormC]
   by_cases h : ¬ BddAbove {x | ∃ i, v (f.coeff R i) * c ^ i = x}; simp [h]
   rw [not_not] at h
   rw [Real.le_sSup_iff h <| gaussNormC_nonempty v c f]
   simp only [Set.mem_setOf_eq, zero_add, exists_exists_eq_and]
   intro ε hε
   use 0
-  simp [lt_of_lt_of_le hε <| apply_nonneg v (f.constantCoeff R)]
+  simpa using lt_of_lt_of_le hε <| apply_nonneg v (f.constantCoeff R)
 
 @[simp]
 theorem gaussNormC_eq_zero_iff [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] {v : F}
-    (h_eq_zero : ∀ x : R, v x = 0 ↔ x = 0) {f : R⟦X⟧} {c : ℝ} (hc : 0 < c)
+    (h_eq_zero : ∀ x : R, v x = 0 → x = 0) {f : R⟦X⟧} {c : ℝ} (hc : 0 < c)
     (hbd : BddAbove {x | ∃ i, v (f.coeff R i) * c ^ i = x})  :
     f.gaussNormC v c = 0 ↔ f = 0 := by
   refine ⟨?_, fun hf ↦ by simp [hf]⟩
   contrapose!
   intro hf
-  simp only [gaussNormC]
   apply ne_of_gt
   obtain ⟨n, hn⟩ := exists_coeff_ne_zero_iff_ne_zero.mpr hf
   calc
@@ -51,5 +72,37 @@ theorem gaussNormC_eq_zero_iff [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ] {
     intro ε hε
     use n
     simp [hε]
+
+end PowerSeries
+
+--#min_imports
+
+#exit
+
+
+--This is false
+theorem isNonarchimedean_gaussNormC [ZeroHomClass F R ℝ] [NonnegHomClass F R ℝ]
+    (hna : IsNonarchimedean v)
+    (f : R⟦X⟧ ) {c : ℝ} (hc : 0 ≤ c) :
+    IsNonarchimedean (gaussNormC v c) := by
+  intro f g
+  rw [gaussNormC]
+  apply Real.sSup_le _ (by simp)
+  intro x hx
+  obtain ⟨i, hi⟩ := hx
+  rw [← hi, map_add]
+  calc
+  v ((coeff R i) f + (coeff R i) g) * c ^ i ≤
+    max (v ((coeff R i) f)) (v ((coeff R i) g)) * c ^ i := by
+    gcongr
+    exact hna ((coeff R i) f) ((coeff R i) g)
+  _ = max (v ((coeff R i) f) * c ^ i) (v ((coeff R i) g) * c ^ i) :=
+    max_mul_of_nonneg _ _ (pow_nonneg hc _)
+  _ ≤ max (gaussNormC v c f) (gaussNormC v c g) := by
+    gcongr
+    sorry
+    sorry
+
+
 
 end PowerSeries
