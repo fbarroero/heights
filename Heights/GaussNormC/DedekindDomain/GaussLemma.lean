dd19066ddb58f0f10b7ac1ec12b2cd5bdc7ba863
fbarroero <fbarroero@gmail.com>
2025-06-16 23:52:43 +0200

    imports GaussLemma

--- Heights/GaussNormC/DedekindDomain/GaussLemma.lean
+++ Heights/GaussNormC/DedekindDomain/GaussLemma.lean
@@ -4,7 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Fabrizio Barroero
 -/
 
-import Mathlib
+import Mathlib.Analysis.RCLike.Basic
+import Mathlib.RingTheory.DedekindDomain.AdicValuation
+import Mathlib.RingTheory.Polynomial.ContentIdeal
+import Mathlib.RingTheory.Polynomial.GaussNorm
 
 /-!
 
